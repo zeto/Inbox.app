2ad83f1902e1d593eb180ea5ac2927eee6efe1d8
Jose Goncalves <zetoeu@gmail.com>
2015-06-02 13:32:41 +0100

    Initial Commit

---
+++ Inbox/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Inbox
+//
+//  Created by Jose Goncalves on 02/06/2015.
+//  Copyright (c) 2015 Jose Goncalves. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

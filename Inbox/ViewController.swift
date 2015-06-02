5f1cf4449fc0f0057a68947dbe94125e067337c7
Jose Goncalves <zetoeu@gmail.com>
2015-06-02 15:32:53 +0100

    Add behaviour to load inbox.google.com

--- Inbox/ViewController.swift
+++ Inbox/ViewController.swift
@@ -7,13 +7,18 @@
 //
 
 import Cocoa
+import WebKit
 
 class ViewController: NSViewController {
 
+    @IBOutlet var webview: WebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        webview.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/8.0.3 Safari/7046A194A"
+        var url = NSURL(string: "https://inbox.google.com")
+        var request = NSURLRequest(URL: url!)
+        webview.mainFrame.loadRequest(request)
     }
 
     override var representedObject: AnyObject? {
@@ -21,7 +26,6 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
 
 }
 
